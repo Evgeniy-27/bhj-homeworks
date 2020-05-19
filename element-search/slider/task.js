123a2e985d7db5aeba70db5307929c1b20e4480b
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-05-20 01:01:09 +0700

    element-search

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,10 +1,13 @@
 const sliderItem = document.querySelectorAll('.slider__item');
 const arrowPrev = document.querySelector('.slider__arrow_prev');
 const arrowNext = document.querySelector('.slider__arrow_next');
+const sliderDot = document.querySelectorAll('.slider__dot');
 let sliderNumber = 0;
 
 function switchSlide(i) {
     sliderItem[sliderNumber].classList.remove("slider__item_active");
+    sliderDot[sliderNumber].classList.remove("slider__dot_active");
+
     sliderNumber = i;
     if (sliderNumber === sliderItem.length) {
         sliderNumber = 0;
@@ -12,6 +15,7 @@ function switchSlide(i) {
         sliderNumber = sliderItem.length - 1;
     }
     sliderItem[sliderNumber].classList.add("slider__item_active");
+    sliderDot[sliderNumber].classList.add("slider__dot_active");
 }
 
 arrowNext.onclick = function () {
@@ -21,3 +25,9 @@ arrowNext.onclick = function () {
 arrowPrev.onclick = function () {
     switchSlide(sliderNumber - 1);
 };
+
+sliderDot.forEach((dot, i) => {
+    dot.onclick = function () {
+        switchSlide(i);
+    };
+});
