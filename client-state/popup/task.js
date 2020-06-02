0a62c0507984c35cfb29ba396b299d8f9d20bfd2
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-06-02 22:30:52 +0700

    Изменения client-state/popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,10 +1,11 @@
 const modal = document.getElementById("subscribe-modal");
 const modalClose = document.querySelector(".modal__close");
 
+console.log(document.cookie.includes('modalClosed=modalactive'));
 console.log(document.cookie);
 
 window.onload = () => {
-    if (document.cookie != 'modalClosed=modalactive') {
+    if (document.cookie.includes('modalClosed=modalactive') === false) {
         modal.classList.add('modal_active');
     }
 };
